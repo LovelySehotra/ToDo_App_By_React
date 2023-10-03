8e3dbd1b4d3441e3ca4bcdcce44e56867b5464ec
Lovely Sehotra <lsehotra12@gmail.com>
2023-10-04 01:08:14 +0530

    update Todoitem

--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,37 +1,62 @@
 import React, { useState } from 'react'
 import { useTodo } from '../Context';
 
-function Todoitems({todo}) {
+function Todoitems({ todo }) {
     const [todoMsg, setTodoMsg] = useState(todo.todo);
-    const[isEditable,setIsEditable] = useState(false);
-    const {updateTodo,deleteTodo,toggleComplete} = useTodo();
+    const [isEditable, setIsEditable] = useState(false);
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo();
 
-    const updatedTodo = (todo)=>{
-        updateTodo(todo.id,{...todo,todo:todoMsg})
+    const updatedTodo = () => {
+        console.log("d")
+        updateTodo(todo.id, { ...todo, todo: todoMsg })
+        setIsEditable(false)
     }
-    const toggleCompleted= (todo)=>{
+    const toggleCompleted = () => {
+        console.log(todo.id)
         toggleComplete(todo.id);
 
     }
-    const deletedTodo = (todo)=>{
+    const deletedTodo = () => {
         deleteTodo(todo.id);
     }
-    
-  return (
-    <div>
-        <input 
-        type="checkbox"
-        checked={todo.complete}
-        onChange={toggleCompleted}
-         />
-        <input 
-        type="text"
-        value={todoMsg} 
-        readOnly/>
-        <button>{isEditable?"save":"edit"}</button>
-        <button>Delete</button>
-    </div>
-  )
+
+    return (
+        <div className=' bg-gray-500 border border-gray-400 gap-4 h-10 flex items-center rounded-xl p-3  '>
+            <div>
+                <input
+                className='w-4 h-4 text-red-600 bg-gray-500 border-gray-500 rounded focus:ring-red-500 dark:focus:ring-red-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 '
+                    type="checkbox"
+                    checked={todo.complete}
+                    onChange={toggleCompleted}
+                />
+           </div>
+            
+           <div>
+                <input
+                className='bg-transparent text-lg font-medium'
+                    type="text"
+                    value={todoMsg}
+                    onChange={(e) => setTodoMsg(e.target.value)}
+                    readOnly={!isEditable} />
+
+            </div>
+
+            <button
+            
+                onClick={() => {
+                    if (todo.complete) return;
+                    if (isEditable) {
+                        updatedTodo();
+                        console.log(todoMsg)
+                    } else {
+                        setIsEditable((prev) => !prev)
+                    }
+                }}
+            >{isEditable ?"📁" : "✏️"}</button>
+            <button
+                onClick={deletedTodo}> ❌</button>
+        </div>
+    )
 }
 
 export default Todoitems
